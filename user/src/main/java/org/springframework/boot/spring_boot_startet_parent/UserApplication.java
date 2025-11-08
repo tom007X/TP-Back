e420a33eb275630a2a8cdad4a9c2b1e9fab24e3c
Benjamin <leguizamonbenjamin0@gmail.com>
2025-11-08 20:20:25 -0300

    Reapply "Creo microservicio de usuarios"
    This reverts commit 6f7f5ae040ee1ddda5ea34dc85611a50105f26cd.

---
+++ user/src/main/java/org/springframework/boot/spring_boot_startet_parent/UserApplication.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.spring_boot_startet_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserApplication.class, args);
+	}
+
+}
---
+++ user/src/test/java/org/springframework/boot/spring_boot_startet_parent/UserApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.spring_boot_startet_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
