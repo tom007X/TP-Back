6d97f4774aeccafd56358ac476a08ef0d189cdc7
Alloatti Uriel <402568@sistemas.frc.utn.edu.ar>
2025-11-15 14:46:38 -0300

    Se remueven trucks y drivers de ShippingMicroservice

--- ShippingMicroservice/src/main/java/com/example/ShippingMicroservice/repository/DriverRepositoryImp.java
+++
@@ -1,8 +0,0 @@
-package com.example.ShippingMicroservice.repository;
-
-import com.example.ShippingMicroservice.model.Driver;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface DriverRepositoryImp extends JpaRepository<Driver,Long> {
-}
