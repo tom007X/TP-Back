08d1d97a268fe15b7df04e63def09694a539851d
Benjamin <leguizamonbenjamin0@gmail.com>
2025-11-14 23:53:42 -0300

    Add redireccionamiento de rutas a partir del api GW. Estructura inicial para el API composition

--- gateway/src/main/java/container_transport/gateway/config/SecurityConfig.java
+++ gateway/src/main/java/container_transport/gateway/config/SecurityConfig.java
@@ -1,61 +1,6 @@
 package container_transport.gateway.config;
 
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.security.oauth2.jwt.Jwt;
-
-import java.util.List;
-import java.util.Map;
-import java.util.stream.Collectors;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AbstractAuthenticationToken;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.core.GrantedAuthority;
-
-@Configuration
-@EnableWebSecurity
 public class SecurityConfig {
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(authorize -> authorize
-            .requestMatchers("/v1/api/public/**")
-            .permitAll()
-            
-            .requestMatchers("/v1/api/hello-1")
-            .hasRole("administrador")
-            
-            .requestMatchers("/v1/api/hello-2")
-            .hasRole("transportista")
-
-            .anyRequest()
-            .authenticated()
-        )
-            .oauth2ResourceServer(oauth2 ->
-                oauth2.jwt(Customizer.withDefaults()));
-                return http.build();
-    }
-
-    @Bean
-    Converter<Jwt, AbstractAuthenticationToken> jwtAuthenticationConverter() {
-        return new Converter<Jwt, AbstractAuthenticationToken>() {
-            @Override
-            public AbstractAuthenticationToken convert(Jwt jwt) {
-                // solicito el claim con el que keycloak manda el mapeo de roles
-                Map<String, List<String>> realmAccess = jwt.getClaim("realm_access");
-                List<GrantedAuthority> authorities = realmAccess.get("roles")
-                    .stream()
-                    .map(r -> "ROLE_" + r)
-                        .map(SimpleGrantedAuthority::new)
-                        .collect(Collectors.toList());
-                return new JwtAuthenticationToken(jwt, authorities);
-            }
-        };
-    }
+    
 }
 
