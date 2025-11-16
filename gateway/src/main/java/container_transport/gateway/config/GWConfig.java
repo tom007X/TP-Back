c2438ff91f53d105ca292231fba379f28bf3ef50
Benjamin <leguizamonbenjamin0@gmail.com>
2025-11-16 12:33:41 -0300

    Configuracion de roles y seguridad

--- gateway/src/main/java/container_transport/gateway/config/GWConfig.java
+++ gateway/src/main/java/container_transport/gateway/config/GWConfig.java
@@ -5,49 +5,22 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-
 
 @Configuration
-@EnableWebFluxSecurity
 public class GWConfig {
+
     @Bean
     public RouteLocator configurarRutas(RouteLocatorBuilder builder,
-    @Value("http://localhost:8090") String uriShipping,
-    @Value("http://localhost:8091") String uriTruck,
-    @Value("http://localhost:8092") String uriClient) {
+                                        @Value("${routing.microservice.trucks}") String uriTrucks,
+                                        @Value("${routing.microservice.clients}") String uriClients,
+                                        @Value("${routing.microservice.shipping}") String uriShipping){
         return builder.routes()
-
-        // TRUCKS MICROSERVICE
-            .route("trucks-route", p -> p
-                .path("/api/v1/trucks/**")
-                .uri(uriTruck)
-                )
-            .route("drivers-route", p -> p
-                .path("/api/v1/drivers/**")
-                .uri(uriTruck)
-                )
-        
-        // CLIENTS MICROSERVICE
-            .route("clients-route", p -> p
-                .path("/api/v1/clients/**")
-                .uri(uriClient)
-                )
-        
-        // SHIPPING MICROSERVICE
-            .route("shipping-addresses-route", p -> p
-                .path("/api/v1/addresses")
-                .uri(uriShipping)
-                )
-            .route("shipping-deposits-route", p -> p
-                .path("/api/v1/deposits")
-                .uri(uriShipping)
-                )
-            .route("shipping-requests-route", p -> p
-                .path("/api/v1/shipping-requests")
-                .uri(uriShipping)
-                )
-                
-            .build();
+                .route(p -> p.path("/api/v1/trucks/**").uri(uriTrucks))
+                .route(p -> p.path("/api/v1/drivers/**").uri(uriTrucks))
+                .route(p -> p.path("/api/v1/clients/**").uri(uriClients))
+                .route(p -> p.path("/api/v1/addresses/**").uri(uriShipping))
+                .route(p -> p.path("/api/v1/deposits/**").uri(uriShipping))
+                .route(p -> p.path("/api/v1/shipping-requests/**").uri(uriShipping))
+                .build();
     }
 }
